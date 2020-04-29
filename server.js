45b742f425e823df95a3fbbcfcd164780e33b7f2
wendelrios <wendelrioss@hotmail.com>
2020-04-29 10:32:57 -0300

    commit new changes

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const {Pool, Client} = require('pg')
+
+// pools will use environment variables
+// for connection information
+const pool = new Pool();
+
+
+
 const app = express();
+
 const port = 3000;
 
 app.listen(port)
