b5a53dcb2a4b073a84648504ba7f5a502c83f574
wendelrios <wendelrioss@hotmail.com>
2020-05-06 23:57:46 -0300

    config database

--- src/database/database.js
+++ src/database/database.js
@@ -1,1 +1,11 @@
 const {Pool, Client} = require('pg')
+
+const pool = new Pool({
+  user:'postgres',
+  host:'localhost',
+  database:'api',
+  password:'wendel12',
+  port:5432
+})
+
+module.exports = {pool}
